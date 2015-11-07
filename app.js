750a461bab90418125aef969317b29cf94991798
Eileen Wei <eileenwei0105@gmail.com>
2015-11-07 13:03:14 -0800

    Update configuration

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var http = require('http').Server(app);
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
